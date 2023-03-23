4bad4c8aed5d83dda0943054393c7ebcd7d1906f
Alexjaca <jalexcardenasa@gmail.com>
2023-03-23 22:24:24 +0100

    register user

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
-
+//IMPORTAR MODELOS
+const user = require("../models/user");
 
 //RUTA DE PRUEBA
 const probando = (req, res) =>{
@@ -8,7 +9,34 @@ const probando = (req, res) =>{
     });
 }
 
+//REGISTRAR USUARIOS /////////////////////////////////////////////////////////////
+const register = (req, res) =>{
+
+    //Recoger los datos de la peticion
+
+    //comprobar que me llegan bien
+
+    //validar los datos
+
+    //control usuarios duplicados
+
+    //cifrar la contraseña
+
+    //crear objeto del ususario
+
+    //guardar usuario en la bbdd
+
+    //limpiar el objeto a devolver
+
+    //devolver un resultado
+    return res.status(200).send({
+        status: "success",
+        message: "Usuario Registrado con exito"
+    });
+}
+
 //EXPORTANDO LAS FUNCIONES DEL CONTROLADOR
 module.exports = {
-    probando
+    probando,
+    register
 }
--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,7 @@ const userController = require("../controllers/user");
 
 //DEFINIR RUTAS
 router.get("/prueba-user", userController.probando);
+router.post("/register", userController.register);
 
 //EXPORTAR ROUTER
 module.exports = router;
