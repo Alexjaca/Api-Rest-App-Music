269985c4820b1a2fc2e96d841741d9dce9242550
Alexjaca <jalexcardenasa@gmail.com>
2023-03-31 13:58:34 +0200

    saver artist

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,14 +1,73 @@
+//IMPORTAR MODELO
+const Artist = require("../models/artist");
 
 
 //RUTA DE PRUEBA
-const probando = (req, res) =>{
+const probando = (req, res) => {
     return res.status(200).send({
         status: "success",
         message: "Mensaje enviado desde: controllers/artist.js"
     });
 }
 
+
+//GUARDAR ARTISTA *************************************************************************
+const save = async (req, res) => {
+    //Recoger los parametros del body
+    const params = req.body;
+    let name = params.name.toLowerCase();
+    params.name = name;
+
+    //Crear el objeto a guardar
+    let artist = new Artist(params);
+
+
+    //Guardar el objeto
+    if (!artist.name) {
+        res.status(400).send({
+            status: "error",
+            message: "No se ha guardado el Artista debe ingresar el nombre"
+        });
+    } else {
+        try {
+
+            const artisFind = await Artist.findOne({ name: artist.name.toLowerCase() });
+
+            if (artisFind) {
+                res.status(400).send({
+                    status: "error",
+                    message: "Este artista ya se encuentra registrado en la BBDD"
+                });
+            } else {
+
+                let artistStored = await artist.save(artist);
+
+                //Devolver resultado
+                res.status(200).send({
+                    status: "success",
+                    message: "Artista registrado correctamente",
+                    artistStored
+                });
+
+            }
+
+        } catch (error) {
+            res.status(404).send({
+                status: "error",
+                message: "Error al Intentar guardar el Artista",
+                error
+            });
+        }
+    }
+
+}
+
+
+
+
+
 //EXPORTANDO LAS FUNCIONES DEL CONTROLADOR
 module.exports = {
-    probando
+    probando,
+    save
 }
--- routes/artist.js
+++ routes/artist.js
@@ -1,6 +1,9 @@
 //IMPORTAR DEPENDENCIAS
 const express = require("express");
 
+//IMPORTAR MIDDLEWARES
+const authorization = require("../middlewares/auth");
+
 //CARGAR ROUTER
 const router = express.Router();    
 
@@ -9,6 +12,12 @@ const artistController = require("../controllers/artist");
 
 //DEFINIR RUTAS
 router.get("/prueba-artist", artistController.probando);
+
+
+//RUTAS REALES
+router.post("/register", authorization.auth, artistController.save);
+
+
 
 //EXPORTAR ROUTER
 module.exports = router;
